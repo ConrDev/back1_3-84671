290ee9d2f19f4d8be2c6b384180ecfd0c6ec62ea
SKELIC <conner@wedevign.nl>
2020-07-06 22:51:52 +0200

    Update artiest_toevoeg_verwerk.php

--- backend/controllers/artiest_toevoeg_verwerk.php
+++ backend/controllers/artiest_toevoeg_verwerk.php
@@ -6,8 +6,6 @@ session_start();
 $artiestnaam = "";
 $instrument = "";
 $gd = "";
-$format = "Y-m-d";
-$datum = date($format, strtotime($gd));
 $geslacht = "";
 $band = "";
 $info = "";
@@ -15,6 +13,7 @@ $errors=[];
 $succes="";
 
 if (isset($_POST['submit'])){
+    // echo $datum;
 
     if (empty($_POST['artiestnaam'])) {
         $errors['artiestnaam'] = 'Vul een artiest naam in';
@@ -23,10 +22,8 @@ if (isset($_POST['submit'])){
     if (empty($_POST['instrument'])) {
         $errors['instrument'] = 'Vul een instrument in';
     }
-
-    if ($gd == $datum) {
-        $errors['gd'] = 'Vul een geldige datum in';
-    } else if (empty($_POST['gd'])) {
+    
+    if (empty($_POST['gd'])) {
         $errors['gd'] = 'Vul een geboortedatum in';
     }
 
@@ -45,9 +42,15 @@ if (isset($_POST['submit'])){
     $artiestnaam = $_POST['artiestnaam'];
     $instrument = $_POST['instrument'];
     $gd = $_POST['gd'];
+    $format = "Y-m-d";
+    $datum = date($format, strtotime($gd));
     $geslacht = $_POST['geslacht'];
     $band = $_POST['bandVeld'];
     $info = htmlentities($_POST ['info'], ENT_QUOTES);
+
+    if ($gd != $datum) {
+        $errors['gd'] = 'Vul een geldige datum in';
+    }
 
     if (count($errors) === 0) {
         $query = "INSERT INTO artiesten SET naam='$artiestnaam', instrument='$instrument', geboortedatum='$gd', geslacht='$geslacht', info='$info', band='$band'";
